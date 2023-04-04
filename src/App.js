a860d0091d63cc7c52ad14bc424b5805facbce6c
AichaAkrout1 <aichadhouha@gmail.com>
2023-04-04 12:01:41 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+import Contact from './components/Contact';
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <Contact/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='nav'>
+        <h2>AICHA AKROUT</h2>
+        <a href="#home">Home</a>
+        <a href="#about">About Me</a>
+        <a href="#menu">Contact</a>
+    </div>
+  )
+}
+
+export default Navbar
